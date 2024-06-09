5629b6031a97888c4efe0c4c5e869de591f93e9d
Benny Chen <benny.jw.chen@gmail.com>
2024-06-09 10:58:36 -0700

    unit test for changing file extensions

--- Test/test.py
+++ Test/test.py
@@ -1,8 +1,18 @@
+import os
 import unittest
+from testUtils import copy_alL_files, check_file_extensions
+from utils import change_files_to_jpg
+
 
 class TestUtilFunctions(unittest.TestCase):
     def test_change_files_to_jpg(self):
-        return
+        original_dir_path = os.path.join(os.getcwd(), "TestChangeFilesToJPG", "Original")
+        test_dir_path = os.path.join(os.getcwd(), "TestChangeFilesToJPG", "Test")
+        copy_alL_files(original_dir_path, test_dir_path)
+        change_files_to_jpg(test_dir_path)
+        self.assertTrue(check_file_extensions(test_dir_path, "jpg"))
+
+
 
 
 if __name__=="main":
